16ef290093bfd21ad012b5c79a59c48bf13bced8
Jeffrey Paxton <pcjnky@users.noreply.github.com>
2022-04-03 13:17:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#print("Hello World!!!!!!")
+#print('I am Iron Man')
+#print("No, I am Tony Stark")
+#print("No, I am Poppy")
+#We are practicing Python...
+
+print("""I am Iron Man
+    No, I am Tony Stark
+No, I am Poppy""")  # triple quotes are like @ in C#
+
+print("I am Iron Man.\n" * 10)
